fce5daed5fe911cf1041ebfd6aa242faa0570bef
King Frigg <kingsleyappiah2143@gmail.com>
2023-12-16 10:58:06 +0000

    added font weight to the center text

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -11,7 +11,9 @@ class Home extends StatelessWidget {
       ),
       body: const Center(child: Text('Im home', style: TextStyle(
         fontSize: 20,
-      )),
+        color: Colors.amber, 
+        fontWeight: FontWeight.bold,
+      ),),
       ),
     );
   }
